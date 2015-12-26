fc65886155930e603667e5d11577ce1ddc4dad48
Кирилл Николаенко <panuka@MacBook-Pro-Kirill.local>
2015-12-26 13:21:36 +0300

    init

---
+++ application/libraries/Twig.php
@@ -0,0 +1,36 @@
+<?php if (!defined('BASEPATH')) {exit('No direct script access allowed');}
+
+class Twig
+{
+	private $CI;
+	private $_twig;
+	private $_template_dir;
+	private $cache_dir;
+	
+	/**
+	 * Constructor
+	 *
+	 */
+	function __construct()
+	{
+	    $this->CI =& get_instance();
+	    $this->CI->config->load('twig');
+		log_message('debug', "Twig Autoloader Loaded");
+
+		$this->_template_dir = $this->CI->config->item('template_dir');
+		$this->_cache_dir = $this->CI->config->item('cache_dir');
+		$loader = new Twig_Loader_FileSystem($this->_template_dir, $this->_cache_dir);
+
+        $this->_twig = new Twig_Environment($loader);
+		
+	}
+
+	public function render($template, $data = array()) {
+
+        $template = $this->_twig->loadTemplate($template.'.twig');
+
+        return $template->render($data);
+	}
+}
+
+?>
